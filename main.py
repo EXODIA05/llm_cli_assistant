2243b1f6c6ad1f6eb0ce1b09a6609f2b314d0903
EXODIA05 <alanmexodia@gmail.com>
2026-09-03 22:47:31 +0530

    feat: add conversational chat session

--- main.py
+++ main.py
@@ -6,12 +6,29 @@ load_dotenv()
 key =os.getenv("key")
 
 client = genai.Client(api_key=key)
+chat = client.chats.create(model = "gemini-2.5-flash")
+print("******CLI ASSISTANT********")
+print("type /help for commands")
 while True:
     user_input=input("You: ")
-    if user_input =="exit!":
-            break
-    response = client.models.generate_content(model = "gemini-2.5-flash",contents = user_input)
-    print(response.text)    
+
+    if user_input =="/exit":
+        break
+    if user_input=="/help":
+        print("""
+        commands 
+        /help = shows commands,
+        /exit = exit the llm app,
+        /clear = clears the memory
+        """)
+        continue
+        
+    if user_input =="/clear":
+        chat = client.chats.create(model="gemini-2.5-flash")
+        print("memory cleared")
+        continue
+    response = chat.send_message(message = user_input)
+    print(response.text)
     
 
 
